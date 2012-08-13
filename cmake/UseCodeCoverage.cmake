eee36ce7d9b118437c69c234a09650229c1979ee
adakraz <adakraz@gmail.com>
2012-08-13 15:05:38 +0200

    added test coverage build option

--- cmake/UseCodeCoverage.cmake
+++ cmake/UseCodeCoverage.cmake
@@ -14,10 +14,10 @@
 
 #
 #  Copyright (C) 2010 Brad Hards <bradh at frogmouth.net>
+#  Copyright (C) 2012 adakraz <adakraz at gmail.com>
 #
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
 OPTION( ENABLE_CODECOVERAGE "Enable code coverage testing support" )
@@ -26,25 +26,29 @@ if ( ENABLE_CODECOVERAGE )
 
     if ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
         message( WARNING "Code coverage results with an optimised (non-Debug) build may be misleading" )
-    endif ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
+    endif ()
 
     if ( NOT DEFINED CODECOV_OUTPUTFILE )
         set( CODECOV_OUTPUTFILE cmake_coverage.output )
-    endif ( NOT DEFINED CODECOV_OUTPUTFILE )
+    endif ()
 
     if ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
         set( CODECOV_HTMLOUTPUTDIR coverage_results )
-    endif ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
+    endif ()
 
-    if ( CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUCXX )
+    if ( CMAKE_COMPILER_IS_GNUCXX )
         find_program( CODECOV_GCOV gcov )
         find_program( CODECOV_LCOV lcov )
         find_program( CODECOV_GENHTML genhtml )
         add_definitions( -fprofile-arcs -ftest-coverage )
         link_libraries( gcov )
-        set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} --coverage )
+        set( CMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage )
         add_custom_target( coverage_init ALL ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_BINARY_DIR} --output-file ${CODECOV_OUTPUTFILE} --capture --initial )
         add_custom_target( coverage ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_BINARY_DIR} --output-file ${CODECOV_OUTPUTFILE} --capture COMMAND genhtml -o ${CODECOV_HTMLOUTPUTDIR} ${CODECOV_OUTPUTFILE} )
-    endif ( CMAKE_COMPILER_IS_GNUCXX )
+        
+        message(STATUS "Code coverage: ON")
+    else ()
+        message( WARNING "Code coverage cannot be turned on for this compiler" )
+    endif ()
 
-endif (ENABLE_CODECOVERAGE )
+endif () 
