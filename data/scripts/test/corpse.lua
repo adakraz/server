ede65ec7d2a5b8b9d619206afd9b12c3d9a93d35
adakraz <adakraz@gmail.com>
2012-08-15 01:13:05 +0200

    created simple test of registerOnDeath()

--- data/scripts/test/corpse.lua
+++ data/scripts/test/corpse.lua
@@ -1,16 +1,18 @@
-module(..., package.seeall)
 
-local m, t
-function suite_setup()
-   m = createMonster('wolf', test_origin)
-   t = m:getParentTile()
-   m:setHealth(0)
-end
 
 function test_dead()
-   local corpse = t:getThing(-1)
-   print(corpse)
-   assert_equal('dead wolf', corpse:getName())
-   assert_equal(0, corpse:size())
-   assert_equal(5, corpse:capacity())
+    local m, t
+    set_time_limit(3000)
+    
+    m = createMonster('wolf', test_origin)
+    t = m:getParentTile()
+    local res = registerOnDeath('all', 'all',
+        function(event)
+            local corpse = t:getThing(-1)
+
+            assert_equal('dead wolf', corpse:getName())
+            assert_equal(0, corpse:size())
+            assert_equal(5, corpse:capacity())
+    end)
+    m:setHealth(0)
 end
--- data/scripts/test/test.lua
+++ data/scripts/test/test.lua
@@ -1,32 +1,34 @@
 require "test.lunatest"
 
-require 'test.suite'
-lunatest.suite('test.corpse')
+local time_limit = 3000
+function set_time_limit(ms)
+    time_limit = ms
+end
+
+
+--require 'test.suite'
+require('test.corpse')
 
 local only_run_tests = getGlobalValue('ONLY_RUN_TESTS') == 'TRUE'
 
 test_origin = {x=100, y=115, z=7}
 
-local hooks = {}
-function hooks.pre_test()
-    -- HACK: terrible hack to wait for all events to fire 
-    wait(2000)
-end
 local function run_all()
-    lunatest.run(hooks)
+    lunatest.run()
 end
 
-local Test = Command:new("Test")
-Test.words = "/test"
-Test.groups = {"Gamemaster", "Senior Gamemaster", "Community Manager", "Server Administrator"}
-function Test.handler(event)
+local TestCmd = Command:new("Test")
+TestCmd.words = "/test"
+TestCmd.groups = {"Gamemaster", "Senior Gamemaster", "Community Manager", "Server Administrator"}
+function TestCmd.handler(event)
 	run_all()
 end
-Test:register()
+TestCmd:register()
 
 if only_run_tests then
     registerOnServerLoad(function()
         run_all()
+        wait(time_limit)
         setGameState(GAME_STATE_SHUTDOWN)
     end)
 end
