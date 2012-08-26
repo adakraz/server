b89df1fc83742241ba9dede28bd935793154e0a2
adakraz <adakraz@gmail.com>
2012-08-26 22:11:17 +0200

    improved lua test harness

--- data/scripts/test/suite.lua
+++ data/scripts/test/suite.lua
@@ -1,18 +1,25 @@
 
-
-function test_dead()
+function test_on_dead()
     local m, t
     set_time_limit(3000)
     
-    m = createMonster('wolf', test_origin)
+    m = createMonster('wolf', get_free_tile())
     t = m:getParentTile()
     local res = registerOnDeath('all', 'all',
-        function(event)
+        wraperror(function(event)
             local corpse = t:getThing(-1)
+            print(t:getX())
 
             assert_equal('dead wolf', corpse:getName())
             assert_equal(0, corpse:size())
             assert_equal(5, corpse:capacity())
-    end)
+    end))
     m:setHealth(0)
+end
+
+function test_traceback()
+    skip('will fail')
+    debug.traceback(coroutine.running())
+    local msg = 'msg12949392'
+    assert_match(msg, debug.traceback(msg, 3))
 end
--- data/scripts/test/test.lua
+++ data/scripts/test/test.lua
@@ -1,34 +1,65 @@
 require "test.lunatest"
 
-local time_limit = 3000
-function set_time_limit(ms)
-    time_limit = ms
+local test_failed
+
+function is_test_key(k)
+   return type(k) == "string" and k:match("^_*test")
 end
 
+local time_limit = 0
+function set_time_limit(ms)
+    time_limit = math.max(time_limit, ms)
+end
 
---require 'test.suite'
-require('test.corpse')
+local function clearerror(err)
+    if type(err) == 'table' then
+        return err:tostring(debug.traceback(coroutine.running()))
+    end
+    return err
+end
+
+function wraperror(fun)
+    return function()
+        
+        ok, err = xpcall(fun, clearerror)
+        if not ok then
+            print(tostring(err))
+            test_failed = true
+        end
+    end
+end
+
+require('test.suite')
 
 local only_run_tests = getGlobalValue('ONLY_RUN_TESTS') == 'TRUE'
 
-test_origin = {x=100, y=115, z=7}
+local test_origin = {x=100, y=115, z=7}
+function get_free_tile()
+    local o = test_origin
+    test_origin.x = test_origin.x + 1
+    return {x=o.x, y=o.y, z=o.z}
+end
 
 local function run_all()
+    test_failed = false
     lunatest.run()
+    wait(time_limit)
+    if test_failed then
+        os.exit(1)
+    end
 end
 
 local TestCmd = Command:new("Test")
 TestCmd.words = "/test"
 TestCmd.groups = {"Gamemaster", "Senior Gamemaster", "Community Manager", "Server Administrator"}
 function TestCmd.handler(event)
-	run_all()
+    run_all()
 end
 TestCmd:register()
 
 if only_run_tests then
     registerOnServerLoad(function()
         run_all()
-        wait(time_limit)
         setGameState(GAME_STATE_SHUTDOWN)
     end)
 end
